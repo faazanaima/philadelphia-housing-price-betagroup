b96b9411ca263951d5280e361a2b760440bb01ef
slash <ifafaaza@gmail.com>
2025-07-24 09:46:11 +0700

    update render.yaml

--- conn.py
+++ conn.py
@@ -5,11 +5,11 @@ import streamlit as st
 def get_connection():
     try:
         conn = psycopg2.connect(
-            user=st.secrets["POOLER_USER"],
-            password=st.secrets["POOLER_PASSWORD"],
-            host=st.secrets["POOLER_HOST"],
-            port=st.secrets["POOLER_PORT"],
-            dbname=st.secrets["POOLER_DATABASE"]
+            user=os.environ["POOLER_USER"],
+            password=os.environ["POOLER_PASSWORD"],
+            host=os.environ["POOLER_HOST"],
+            port=os.environ["POOLER_PORT"],
+            dbname=os.environ["POOLER_DATABASE"]
         )
         # st.success("✅ Connected successfully.")
         return conn
@@ -18,3 +18,20 @@ def get_connection():
         st.error("❌ Failed to connect.")
         st.exception(e)
         return None
+
+# def get_connection():
+#     try:
+#         conn = psycopg2.connect(
+#             user=st.secrets["POOLER_USER"],
+#             password=st.secrets["POOLER_PASSWORD"],
+#             host=st.secrets["POOLER_HOST"],
+#             port=st.secrets["POOLER_PORT"],
+#             dbname=st.secrets["POOLER_DATABASE"]
+#         )
+#         # st.success("✅ Connected successfully.")
+#         return conn
+
+#     except OperationalError as e:
+#         st.error("❌ Failed to connect.")
+#         st.exception(e)
+#         return None
